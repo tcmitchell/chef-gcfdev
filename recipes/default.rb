2af25000170da21c311e6e242318f8905c4751cf
Tom Mitchell <tmitchell@bbn.com>
2014-11-06 20:52:50 -0500

    Populate default recipe.
    Install dependencies to prep for gcf development

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,46 @@
 # Cookbook Name:: gcfdev
 # Recipe:: default
 #
-# Copyright (c) 2014 The Authors, All Rights Reserved.
+
+# -----------------------------------------------------------------
+# Note: each of these could be their own recipe to handle platform
+# differences like package name on different distributions, or package
+# name changes across distribution versions.
+# -----------------------------------------------------------------
+
+package 'm2crypto' do
+  action :install
+end
+
+package 'python-dateutil' do
+  action :install
+end
+
+package 'pyOpenSSL' do
+  action :install
+end
+
+package 'xmlsec1' do
+  action :install
+end
+
+package 'xmlsec1-devel' do
+  action :install
+end
+
+package 'xmlsec1-openssl' do
+  action :install
+end
+
+package 'xmlsec1-openssl-devel' do
+  action :install
+end
+
+package 'python-importlib' do
+  action :install
+end
+
+package 'python-lxml' do
+  action :install
+end
+
